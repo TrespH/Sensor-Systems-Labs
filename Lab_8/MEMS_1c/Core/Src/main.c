99e526e4a79c72ec941d92d7e0a81867b1808b15
Andre-maf <andrea.maffezzini@mail.polimi.it>
2024-11-06 16:07:13 +0100

    mems_1c update, functioning readout, tested

--- Lab_8/MEMS_1c/Core/Src/main.c
+++ Lab_8/MEMS_1c/Core/Src/main.c
@@ -63,13 +63,13 @@ uint8_t CTRL_REG4[] = {0x23, 0b00000000}; //reg address, continuos update + 2g F
 uint16_t MEMS_REGISTER_X_AUTO_INCREMENT = 0x29 | 0x80; // Setting MSB to 1 to enable auto-increment
 
 uint16_t size = 1; // #Bytes of initial I2C transmissions
-uint16_t multiple_size = 3; // #Bytes to read (X, Y, Z)
+uint16_t multiple_size = 5; // #Bytes to read (X, Y, Z)
 uint32_t timeout = 10; // Timeout for initial I2C transmissions
 
 char string[32];
 int string_length = 0;
 
-int8_t xyz_data[3]; // Array to hold X, Y, Z data
+int8_t xyz_data[5]; // Array to hold X, Y, Z data
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -87,10 +87,10 @@ static void MX_TIM2_Init(void);
 /* USER CODE BEGIN 0 */
 void HAL_TIM_PeriodElapsedCallback (TIM_HandleTypeDef *htim) {
 	xyz_data[0] = 0;
-	xyz_data[1] = 0;
 	xyz_data[2] = 0;
+	xyz_data[4] = 0;
     // Transmit the X register address with auto-increment enabled
-	HAL_I2C_Master_Transmit_DMA(&hi2c1, MEMS_WR_ADDRESS, (uint8_t*)&MEMS_REGISTER_X_AUTO_INCREMENT, multiple_size);
+	HAL_I2C_Master_Transmit_DMA(&hi2c1, MEMS_WR_ADDRESS, (uint8_t*)&MEMS_REGISTER_X_AUTO_INCREMENT, 1);
 }
 
 void HAL_I2C_MasterTxCpltCallback (I2C_HandleTypeDef *hi2c){
@@ -100,8 +100,8 @@ void HAL_I2C_MasterTxCpltCallback (I2C_HandleTypeDef *hi2c){
 
 void HAL_I2C_MasterRxCpltCallback (I2C_HandleTypeDef *hi2c){
 	float acc_g_x = xyz_data[0] / 64.0;
-	float acc_g_y = xyz_data[1] / 64.0;
-	float acc_g_z = xyz_data[2] / 64.0;
+	float acc_g_y = xyz_data[2] / 64.0;
+	float acc_g_z = xyz_data[4] / 64.0;
 	string_length = snprintf(string, sizeof(string), "X: %.2f, Y: %.2f, Z: %.2f\n", acc_g_x, acc_g_y, acc_g_z);
 	HAL_UART_Transmit_DMA(&huart2, (uint8_t*)string, string_length);
 }
